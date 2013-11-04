64ecd497bce77819f4142cecde742a503b9f8fc6
grepwood <grepwood@sucs.org>
2013-11-04 13:02:48 +0000

    Correcting src/help.c

--- src/help.c
+++ src/help.c
@@ -2,5 +2,5 @@
 
 void help(char * exe)
 {
-	printf("brb-universe\nCopyright 2013 grepwood, licensed under GPL v3\n\nUsage: %s\t[help || sec length]\n\t-h, --help\tprint this message\n\tsec\t\tsecurity for which to generate\n\tlen\t\tlength of generated password\n", exe);
+	printf("brb-universe\nCopyright 2013 grepwood, licensed under GPL v3\n\nUsage: %s\t[help || sec len]\n\t-h, --help\tprint this message\n\tsec\t\tsecurity for which to generate\n\tlen\t\tlength of generated password\n", exe);
 }
